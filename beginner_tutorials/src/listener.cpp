cd1aa7a57389e639de7e7543be681f54cb44ec64
orestis <orestis@ee.auth.gr>
2014-04-30 13:54:34 +0300

    12

--- beginner_tutorials/src/listener.cpp
+++ beginner_tutorials/src/listener.cpp
@@ -42,7 +42,7 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
 int main(int argc, char **argv)
 {
   /**
-   * The ros::init() function needs to see argc and argv so that it can performs
+   * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line. For programmatic
    * remappings you can use a different version of init() which takes remappings
    * directly, but for most command-line programs, passing argc and argv is the easiest
